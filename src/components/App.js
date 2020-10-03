459aa59d2c0a17d7cb435f5d157008bdb03a5d2b
Martin <martin.guzlej@gmail.com>
2020-10-03 19:26:38 +0100

    successfully requested for unsplash API

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
     onSearchSubmit(term) {
-        
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID YGGMc3qjq5OLyki5BXQAKCV9ZMkJd59c7LRUI6kVlb0'
+            }
+        });
     }
     render() {
         return (
